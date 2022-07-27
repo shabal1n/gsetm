aa1a46acdefa2ce9bacd232aff7cc700d6da3b39
shabal1n <artur.shab@yandex.ru>
2022-07-27 18:35:31 +0600

    About the company page is ready

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.main_page, name='main_page'),
+    path('about', views.about_company, name='about_company'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def main_page(request):
     return render(request, 'main_page.html')
+
+
+def about_company(request):
+    return render(request, 'about_company.html')
