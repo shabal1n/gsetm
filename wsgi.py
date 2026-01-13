dc121719bd60dc5261214b9bb69e93dcc54eefed
Artur Shabalin <artur.shab@yandex.ru>
2026-01-13 23:28:41 +0500

    Fixed deprecated method

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,13 @@
 import os
 import sys
 
-# Jelastic virtualenv path - ИСПРАВЛЕНО
+# Jelastic virtualenv path
 virtenv = '/var/www/webroot/virtenv'
 activate_this = os.path.join(virtenv, 'bin', 'activate_this.py')
 
 if os.path.exists(activate_this):
-    execfile(activate_this, dict(__file__=activate_this))
+    with open(activate_this) as f:
+        exec(f.read(), {'__file__': activate_this})
 
 # Add project to path
 project_dir = '/var/www/webroot/ROOT'
@@ -14,7 +15,7 @@ sys.path.insert(0, project_dir)
 sys.path.insert(0, '/var/www/webroot')
 
 # Django settings
-os.environ['DJANGO_SETTINGS_MODULE'] = 'gsetm.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gsetm.settings')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
