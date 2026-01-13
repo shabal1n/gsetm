1b0fab01cf064b89b5ced010fb0d95be2f98041c
Artur Shabalin <artur.shab@yandex.ru>
2026-01-13 23:02:02 +0500

    Fixed wsgi env path

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,17 @@
 import os
 import sys
 
-# Jelastic virtualenv path
-virtenv = os.path.expanduser('~') + '/virtenv'
+# Jelastic virtualenv path - ИСПРАВЛЕНО
+virtenv = '/var/www/webroot/virtenv'
 activate_this = os.path.join(virtenv, 'bin', 'activate_this.py')
 
 if os.path.exists(activate_this):
     execfile(activate_this, dict(__file__=activate_this))
 
 # Add project to path
-project_dir = os.path.expanduser('~') + '/ROOT'
+project_dir = '/var/www/webroot/ROOT'
 sys.path.insert(0, project_dir)
-sys.path.insert(0, os.path.expanduser('~'))
+sys.path.insert(0, '/var/www/webroot')
 
 # Django settings
 os.environ['DJANGO_SETTINGS_MODULE'] = 'gsetm.settings'
