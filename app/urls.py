4cb07b663fe264edce51af1a7f613499ee572405
Artur Shabalin <artur.shab@yandex.ru>
2026-01-18 16:55:23 +0500

    Ready for prod

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
+from django.views.generic.base import TemplateView
 from .sitemaps import StaticViewSitemap
 
 sitemaps = {
@@ -11,15 +12,24 @@ sitemaps = {
 
 urlpatterns = [
     path("", views.main_page, name="Главная"),
-    path("about/", views.about_company, name="О компания"),
-    path("generators/", views.generators, name="Генераторы"),
-    path("generator/<int:gen_id>/", views.generator_description),
-    path("rent/", views.rent, name="Аренда"),
-    path("projects/", views.projects, name="Наши проекты"),
-    path("contacts/", views.contacts, name="Контакты"),
-    path("category/<int:category_id>/", views.generators_category),
-    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
-]
+    path("about", views.about_company, name="О компании"),
+    path("generators", views.generators, name="Генераторы"),
+    path("generators/<int:category_id>", views.generators_category, name="Генераторы"),
+    path("generator/<int:gen_id>", views.generator_description, name="Генератор"),
+    path("rent", views.rent, name="Аренда"),
+    path("projects", views.projects, name="Наши проекты"),
+    path("contacts", views.contacts, name="Контакты"),
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- gsetm/settings.py
+++ gsetm/settings.py
@@ -28,14 +28,14 @@ CSRF_TRUSTED_ORIGINS = [
 SECRET_KEY = "django-insecure-ftidn9@h37(cg(9ly+w6(^^0@gd96uv(7h@0zvl&x%=t&&wg#z"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "gsetm.kz",
     "www.gsetm.kz",
     "194.39.67.204",
     "gsetm.jcloud.kz",
-    "www.gsetm.jcloud.kz"
+    "www.gsetm.jcloud.kz",
 ]
 
 # Application definition
